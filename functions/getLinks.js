7f740bb0ef12d88abdb50141b9830231c92f97d4
Chuck Belcher <cfbelcher@pateogroup.com>
2021-08-10 20:39:44 -0400

    Created function to pull all links from faunadb

---
+++ functions/getLinks.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+require('dotenv').config();
+exports.handler = async (event) => {
+    const GET_LINKS = `
+        query {
+            allLinks {
+                data {
+                    name
+                    _id
+                    url
+                    description
+                    archived
+                }
+            }
+        }`;
+        
+    const {data} = await axios({
+        url: 'https://graphql.us.fauna.com/graphql',
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${process.env.FAUNA_SECRET_KEY}`,
+        },
+        data: {
+            query: GET_LINKS,
+            variables: {}
+        }
+    });
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data)
+    }
+}
+
